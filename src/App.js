bf9c1fdfc346493035d5c3e96fdd19b3d2008187
radithyad <radithya.dhiyaurrahman@gmail.com>
2022-12-05 14:08:44 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import Intro from './Components/Intro'
+import NavigationBar from './Components/NavigationBar';
+import TrendingGame from './Components/TrendingGame';
+import BestGame from './Components/BestGame';
+import Contact from './Components/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='myBG'>
+        <NavigationBar/>
+        <Intro/>
+      </div>
+
+      <div className='trending'>
+        <TrendingGame/>
+      </div>
+
+      <div className='best'>
+        <BestGame/>
+      </div>
     </div>
+
   );
 }
 
